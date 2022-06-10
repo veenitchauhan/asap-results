2903ce7f3e24a9d24d277425c71b899abe2a3b70
Veenit Chauhan <veenitchauhan@gmail.com>
2022-06-10 19:48:33 +0530

    note mentioned

--- resources/views/patient_collection/create.blade.php
+++ resources/views/patient_collection/create.blade.php
@@ -4,6 +4,9 @@
 <div class="content">
     <div class="container-fluid">
         <div class="col-md-12">
+            <div class="alert alert-secondary" role="alert">
+                This form is under development for input fields, color scheme and backend processing is not done yet.
+            </div>
 
             <a href="{{ route('collection.index') }}">
                 <button type="button" class="btn btn-secondry" data-toggle="modal" data-target="#addNewUserModal">
@@ -18,106 +21,106 @@
                 </div>
 
                 <div class="card-body">
-                <form>
-					@method('PUT') @csrf
-					<input type="hidden" name="id">
-					<div class="form-group">
-						<label>Clinic Req #: </label>
-						<input type="text" name="" class="form-control" placeholder="Clinic Req#">
-					</div>
-					<div class="form-group">
-						<label>Select Test Type: </label>
-						<input type="ematextil" name="contant_person" class="form-control" placeholder="Enter name of contact person">
-					</div>
-					<div class="form-group">
-						<label>LAB: </label>
-						<input type="ematextil" name="contact_number" class="form-control" placeholder="Enter contact number">
-					</div>
-					<div class="form-group">
-						<label>First Name: </label>
-						<input type="ematextil" name="clinic" class="form-control" placeholder="Enter name of clinic">
-					</div>
-					<div class="form-group">
-						<label>Last Name: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>DOB: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Is Minor: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Gender: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Address: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Address 2: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>City: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>State: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Zip: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Phone Number: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Email: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Choose Race: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Choose Ethnicity: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>SSN/Driver/Passport ID/Other: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Select ID No.: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>State listed on selected ID: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Insurance Provider: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Insurance Policy #: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Insurance Eligibility Verification #: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-					<div class="form-group">
-						<label>Insurance Discovery: </label>
-						<input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
-					</div>
-				</form>
+                    <form>
+                        @method('PUT') @csrf
+                        <input type="hidden" name="id">
+                        <div class="form-group">
+                            <label>Clinic Req #: </label>
+                            <input type="text" name="" class="form-control" placeholder="Clinic Req#">
+                        </div>
+                        <div class="form-group">
+                            <label>Select Test Type: </label>
+                            <input type="ematextil" name="contant_person" class="form-control" placeholder="Enter name of contact person">
+                        </div>
+                        <div class="form-group">
+                            <label>LAB: </label>
+                            <input type="ematextil" name="contact_number" class="form-control" placeholder="Enter contact number">
+                        </div>
+                        <div class="form-group">
+                            <label>First Name: </label>
+                            <input type="ematextil" name="clinic" class="form-control" placeholder="Enter name of clinic">
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>DOB: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Is Minor: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Gender: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Address: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Address 2: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>City: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>State: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Zip: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Phone Number: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Email: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Choose Race: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Choose Ethnicity: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>SSN/Driver/Passport ID/Other: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Select ID No.: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>State listed on selected ID: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Insurance Provider: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Insurance Policy #: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Insurance Eligibility Verification #: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                        <div class="form-group">
+                            <label>Insurance Discovery: </label>
+                            <input type="ematextil" name="timezone" class="form-control" placeholder="Enter timezone">
+                        </div>
+                    </form>
 
                 </div>
 
